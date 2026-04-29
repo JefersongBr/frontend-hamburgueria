69fec3fccc6e0729f1cf96f11cb2b6e90659e8f3
JefersongBr <micaelcarvalhopds@gmail.com>
2026-04-28 22:47:29 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Burguer</h1>
+      <p>Bem-Vindo à Burguer!</p>
+
+      <button> clique aqui</button>
+    </div>
+  );
+}
+
+export default App;
